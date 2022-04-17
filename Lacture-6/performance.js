9fdaaee9879b2cac67fce63a3c7c988564c2c137
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-04-17 11:23:51 +0600

    delete performance check

--- Lacture-6/performance.js
+++ Lacture-6/performance.js
@@ -12,10 +12,10 @@ for(let i = 0; i<5000000; i++){
 
 console.time('find');
 const id = 4999999;
-const idtoBeUpdated = arr.find(item => item.id === id);
-idtoBeUpdated.value = 555;
+const idtoBeUpdated = arr.findIndex(item => item.id === id);
+arr.splice(id,1)
 console.timeEnd('find');
 
 console.time('obj');
-obj[id].value = 999;
+delete obj[id];
 console.timeEnd('obj');
