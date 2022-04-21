3a19821b929a2b215c436a3bc7e93cd4956dd8e0
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-04-21 18:23:12 +0600

    functional programming understanding starting

--- Lecture-8/lecture-8/whyReturn.js
+++ Lecture-8/lecture-8/whyReturn.js
@@ -10,9 +10,29 @@ function multiplication(a,b){
     return a * b;
 }
 
-const a = 14;
-const b = 25;
+const a = 2;
+const b = 4;
 
-const result = multiplication(sum(a,b) - sub(a,b));
+const result = Math.abs(multiplication(sum(a,b), sub(a,b)));
+
+/*  এখানে প্রথমে sum এবং sub এর return, multiplication তার নিজের value অথবা argument হিসেবে ব্যবহার করেছে। আবার multiplication এর return, Math.abs() argument হিসেবে ব্যবহার করেছে। */
 
 console.log(result);
+
+/* 
+ - Function defination
+ - Function invoking
+ - Function Parameters/ Arguments
+ - Return result from function
+*/
+
+/* 
+What is functional Progrmming?
+    Answer: Funciton is a first class citizen
+        What is First Class Citizen?
+            Answer: we can treat function as value
+            যেসব প্রোগ্রামিং ল্যাংগুয়েজের ফাংশনকে ভ্যালু হিসেবে পাস করা যায় সে সকল প্রোগ্রামিং ল্যাঙ্গুয়েজ কে ফাংশনাল প্রগ্রমিং লঙ্গুয়েজ বলে।
+
+            value is 10, "text", true -> function
+            value বলতে কেমন মনে করতে পারি সেটা হচ্ছে যে আমরা একটা নম্বর কে, টেক্সটকে আমরা যেভাবে মনে করি ঠিক সেভাবেই ভ্যালু হিসেবে ফাংশনকে মনে করতে যদি পারা যায় তাহলে আসলেই ফাংশনাল প্রগ্রমিং বলা যাবে।
+*/
