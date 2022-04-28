ef287a19ce7bb1c26239bc7b8fd69d098d8336ae
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-04-28 17:40:38 +0600

    heigher order function with paly reture a function

--- Lecture-9/heigherOrderfun.js
+++ Lecture-9/heigherOrderfun.js
@@ -32,3 +32,31 @@ console.log(gererateTwoRandomNumber(100, (rend1, rend2) => rend1 + rend2));
 console.log(gererateTwoRandomNumber(10, (rend1, rend2) => rend1 - rend2));
 console.log(gererateTwoRandomNumber(50, (rend1, rend2) => rend1 * rend2 + rend2 * rend2));
 
+
+
+function power(p){
+    return function (n){
+        let  resultOfPower = 1;
+        for (let i = 1; i <= p; i++) {
+            resultOfPower *= n;
+        }
+        return resultOfPower;
+    } 
+}
+
+const sqr /* sqr = function (n){
+    let  resultOfPower = 1;
+    for (let i = 1; i <= p; i++) {
+        resultOfPower *= n;
+    }
+    return resultOfPower;
+}  */ = power(2);
+
+const cube = power(3);
+const power8 = power(8);
+
+console.log("SQR", sqr(4));
+console.log("CUBE", cube(3));
+console.log("POWER8", power8);
+
+console.log(sqr(4))
