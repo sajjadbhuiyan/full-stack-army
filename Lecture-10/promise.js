fc45b69fe2ddd760bf6613e5554c20e44a50b107
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-05-05 10:55:53 +0600

    Promise race method

--- Lecture-10/promise.js
+++ Lecture-10/promise.js
@@ -1,3 +1,24 @@
+const promise1 = new Promise((resolve, reject) =>{
+    setTimeout(()=>{
+        resolve("Prominse 1 Resolved");
+    },5000)
+})
+
+const promise2 = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        resolve("Promise 2 Resolved");
+    }, 2000)
+})
+
+Promise.race([promise1, promise2]).then((result)=>{
+        console.log(result);
+    })
+
+
+
+/* 
+======PROMISE ALL METHOD======
+
 const promise1 = Promise.resolve("Prominse 1 Resolved");
 
 const promise2 = new Promise((resolve, reject) =>{
@@ -9,9 +30,10 @@ const promise2 = new Promise((resolve, reject) =>{
 Promise.all([promise1, promise2])
     .then((result)=>{
         console.log(result);
-    })
+    }) */
 
 
+// ******UNDERTANDING OF PROMISE******
 
 /* const hasMeeting = false;
 
