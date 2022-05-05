fe1fc1acd856abaab183f0a6a1edf1153f7e3083
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-05-05 11:32:13 +0600

    async await simple understanding

---
+++ Lecture-10/async-await.js
@@ -0,0 +1,38 @@
+const hasMeeting = false;
+
+const meeting = new Promise((resolve, reject) =>{
+    if (!hasMeeting) {
+        const meetingDatails = {
+            name: "CPE Meeting",
+            location: "Google Meet",
+            time: "10: 00 PM",
+        }
+
+        resolve(meetingDatails)
+    }else{
+        reject(new Error("Meeting already scheduled!"))
+    }
+})
+
+
+const addToCalender = (meetingDetails) =>{
+    // meetingDetails means ---
+    // const meetingDatails = {
+    //     name: "CPE Meeting",
+    //     location: "Google Meet",
+    //     time: "10: 00 PM",
+    // }
+        const calender = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`
+        return Promise.resolve(calender)
+}
+
+
+async function myMeeting(){
+    const meetingObject = await meeting;
+    const calander = await addToCalender(meetingObject);
+    console.log(calander)
+}
+
+myMeeting();
+
+console.log("Hello")
